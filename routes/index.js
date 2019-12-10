f7472e2c04ed92b925b9447d0df0d8b5c5802b80
sainsh <kfsainsh@gmail.com>
2019-12-10 14:15:45 +0100

    added motion control but does not take picture

--- routes/index.js
+++ routes/index.js
@@ -13,34 +13,47 @@ const myCamera = new PiCamera({
   width: 640,
   height: 480,
   nopreview: true,
-});
-const path = require('path');
-const MotionDetectionModule = require('pi-motion-detection');
-const motionDetector = new MotionDetectionModule({
-  captureDirectory: path.resolve(__dirname),
-});
+});/*
+var PiMotion = require('node-pi-motion');
 
-motionDetector.on('motion', () => {
-  console.log('motion!');
-  myCamera.snap()
+var options = {
+  throttle: 10000
+}
+
+var nodePiMotion = new PiMotion(options);
+
+nodePiMotion.on('DetectedMotion', async function() {
+  console.log('Motion detected! Now do something.');
+  console.log("taking picture")
+  await  myCamera.snap()
   .then(async(result) => {
+    console.log("picture taken");
     var color = await colorReg.findColor(__dirname + '/test.jpg');
     console.log(color);
     fs.unlinkSync(__dirname + '/test.jpg');
-    //res.render('index', {title: 'ColorReg', color: color})
+    if(color == "red")ledRed.digitalWrite(1)
+    if(color == "green")ledGreen.digitalWrite(1)
+    if(color == "blue")ledBlue.digitalWrite(1)
   })
   .catch((error) => {
      // Handle your error
   });
 });
- 
-motionDetector.on('error', (error) => {
-  console.log(error);
-});
- 
-motionDetector.watch();
 
+var Gpio = require('pigpio').Gpio, //include pigpio to interact with the GPIO
+ledRed = new Gpio(4, {mode: Gpio.OUTPUT}), //use GPIO pin 4 as output for RED
+ledGreen = new Gpio(17, {mode: Gpio.OUTPUT}), //use GPIO pin 17 as output for GREEN
+ledBlue = new Gpio(27, {mode: Gpio.OUTPUT}), //use GPIO pin 27 as output for BLUE
+redRGB = 0, //set starting value of RED variable to off (0 for common cathode)
+greenRGB = 0, //set starting value of GREEN variable to off (0 for common cathode)
+blueRGB = 0; //set starting value of BLUE variable to off (0 for common cathode)
 
+//RESET RGB LED
+ledRed.digitalWrite(0); // Turn RED LED off
+ledGreen.digitalWrite(0); // Turn GREEN LED off
+ledBlue.digitalWrite(0); // Turn BLUE LED off
+
+*/
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'ColorReg' });
@@ -65,4 +78,12 @@ router.post('/pic/', async (req,res,next) =>{
   });
 })
 
+process.on('SIGINT', function () { //on ctrl+c
+  ledRed.digitalWrite(0); // Turn RED LED off
+  ledGreen.digitalWrite(0); // Turn GREEN LED off
+  ledBlue.digitalWrite(0); // Turn BLUE LED off
+  process.exit(); //exit completely
+});
+
 module.exports = router;
+
